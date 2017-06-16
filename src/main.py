a96ce3a31ccc0b71c97e488ce8438dca7e67408a
Guilherme Varela <guilhermevarela@hotmail.com>
2017-06-16 16:07:18 -0300

    New saving functions (2)

--- src/main.py
+++ src/main.py
@@ -22,7 +22,7 @@ S = schedule_builder(opponents)
 
 timestamp = int(timeit.time.time())
 
-output_dir = "../ouput/%d/" %(timestamp)
+output_dir = "../output/%d/" %(timestamp)
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
   
